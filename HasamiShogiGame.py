1fb64c9913caa67a3d144234c9127d0ce7b6c274
Steve <65416951+ChocolateTaco@users.noreply.github.com>
2021-11-23 13:02:21 -0800

    Updated some valid moves

--- HasamiShogiGame.py
+++ HasamiShogiGame.py
@@ -10,13 +10,36 @@ class HasamiShogiGame():
         self._columns = 9
         self._top_label = list(range(1,10))
         self._side_label = ['a','b','c','d','e','f','g','h','i']
-        self._game_board = []
+        self._game_board = [["_", "_", "_", "_", "_", "_", "_", "_", "_"],
+            ["_", "_", "_", "_", "_", "_", "_", "_", "_"],
+            ["_", "_", "_", "_", "_", "_", "_", "_", "_"],
+            ["_", "_", "_", "_", "_", "_", "_", "_", "_"],
+            ["_", "_", "_", "_", "_", "_", "_", "_", "_"],
+            ["_", "_", "_", "_", "_", "_", "_", "_", "_"],
+            ["_", "_", "_", "_", "_", "_", "_", "_", "_"],
+            ["_", "_", "_", "_", "_", "_", "_", "_", "_"],
+            ["_", "_", "_", "_", "_", "_", "_", "_", "_"]]
         self._move_tracker = 1      # Tracks the number of moves performed in the game
 
         # Initializes Empty Game Board
+        # for row in range(9):
+        #     for column in self._game_board:
+        #         # print(f"{row}{column}")
+        #         self._game_board.append("_")
+
+    def __repr__(self) -> str:
+        """Printable Format of the Board Game State"""
+        for i in self._side_label:
+            print(self._side_label)
+
+    def display_game(self):
+        """Displays the current state of the game. """
         for row in range(self._rows):
-            for column in range(self._columns):
-                self._game_board[row, column] = "_"
+            # for column in range(self._columns):
+            pass
+            for col in self._side_label:
+                pass
+            print(f"{col} {self._game_board[row]}")
 
     def get_top(self):
         return self._top_label
@@ -24,20 +47,19 @@ class HasamiShogiGame():
     def get_side(self):
         return self._side_label
 
-    def set_RED(self,):
+    def set_RED(self, space):
         """Sets the specified location to RED."""
-        return "RED"
+        if space is None:
+            space = "r"
+            return True
 
     def set_BLACK(self):
-        return "BLACK"
+        if space == "_":
+            space = "b"
+            return True
 
-    def get_square
-
-    def display_game():
-        """Displays the current state of the game. """
-        for row in self._rows:
-            for column in self._columns:
-                print(self._game_board)
+    def set_empty(self):
+        return None
 
     def get_game_state(self):
         """Function determines the current progress of the game. If there's a current winner or if its still in play.
@@ -45,9 +67,9 @@ class HasamiShogiGame():
         Returns:
             str: state of the board game
         """
-        if game_state is None:
+        if game_state == None:
             return "UNFINISHED"
-        elif game_state is "RED_WON":
+        elif game_state == "RED_WON":
             return "RED_WON"
         else:
             return "BLACK_WON"
@@ -64,10 +86,10 @@ class HasamiShogiGame():
             return "RED"
 
     def get_num_captured_pieces(self, color = None):
-        if color.upper() is "RED":
+        if color.upper() == "RED":
             # Returns the number of red pieces captured
             pass
-        if color.upper() is "BLACK":
+        if color.upper() == "BLACK":
             # Returns the number of red pieces captured
             pass
         else:
@@ -86,7 +108,7 @@ class HasamiShogiGame():
 
         Returns:
             True: updated location with the piece. Also updates start_loc to an empty location again. Updates the active player, and move_tracker
-            False: if any moment of the move is invalid
+            False: if the current move is blocked by any pieces
         """
         pass
 
@@ -97,18 +119,71 @@ class HasamiShogiGame():
             pass
         self._row = square[0]
         self._column = square[1]
-        if self._game_board[self._row][self._column] is "BLACK":
+        if self._game_board[self._row][self._column] == "BLACK":
             return "BLACK"
-        elif self._game_board[self._row][self._column] is "RED":
+        elif self._game_board[self._row][self._column] == "RED":
             return "RED"
         else:
             return "NONE"
 
-* A method called `make_move` that takes two parameters - strings that represent the square moved from and the square moved to.  For example, make_move('b3', 'b9').  If the square being moved from does not contain a piece belonging to the player whose turn it is, or if the indicated move is not legal, or if the game has already been won, then it should just return False.  Otherwise it should make the indicated move, remove any captured pieces, update the game state if necessary, update whose turn it is, and return True.
+    ### Verifying Valid Moves
+    def horizontal_move(self, start_loc, end_loc):
+        """Checks to see if there are any pieces between the start_loc to the end_loc
+
+        Args:
+            start_loc (str): a location with an active piece 
+            end_loc (str): a valid location to move an active piece
+
+        Returns:
+            True: move is valid
+            False: move is not valid (something obstructs)
+        """
+        pass
+    def corner_capture(self, start_loc, end_loc):
+        """If the current move is nearby an opponent's corner piece, check to see if another active player's piece resides on the nearby corner. 
+
+        Args:
+            start_loc (str): a location with an active piece 
+            end_loc (str): a valid location to move an active piece
+
+        Returns:
+            True: Captures the corner piece. Updates the active player, and move_tracker
+            False: if corner capture condition is not met
+        """
+        pass
+
+    def horizontal_capture(self, start_loc, end_loc):
+        """After a valid move check, determines if the current move is horizontal to an opponent's piece. If so check to see if another active player's piece is on the opposite side. 
+
+        Args:
+            start_loc (str): a location with an active piece 
+            end_loc (str): a valid location to move an active piece
+
+        Returns:
+            True: Captures the opponent's piece piece. Updates number of captured pieces, and move_tracker
+            False: if horizontal capture condition is not met
+        """
+        pass
+
+    def vertical_capture(self, start_loc, end_loc):
+        """After a valid move check, determines if the current move is vertical to an opponent's piece. If so check to see if another active player's piece is on the opposite side. 
+
+        Args:
+            start_loc (str): a location with an active piece 
+            end_loc (str): a valid location to move an active piece
+
+        Returns:
+            True: Captures the opponent's piece piece. Updates number of captured pieces, and move_tracker
+            False: if vertical capture condition is not met
+        """
+        pass
+
 
 
 game = HasamiShogiGame()
 game.display_game()
+print("repr representation\n")
+print(game)
 
 # move_result = game.make_move('i6', 'e3')
 # print(game.get_active_player())
