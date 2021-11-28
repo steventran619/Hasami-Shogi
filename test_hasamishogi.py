5bac9045eca47cbdadd985b3da1caa6a97f545c3
ChocolateTaco <dpoopd@gmail.com>
2021-11-28 12:02:55 -0800

    Test Pass: Horizontal and Vertical obstructions

--- test_hasamishogi.py
+++ test_hasamishogi.py
@@ -3,35 +3,50 @@ from HasamiShogiGame import *
 
 class HasamiTests(unittest.TestCase):
     """List of tests used during the development of Library.py its functions"""
-    def test01_get_active_player(self):
+    def test01_vertical_move(self):
         """This test ensures that the turns alternate after a successful move."""
         game = HasamiShogiGame()
         player = game.get_active_player()
         self.assertEqual(player, "BLACK")
-        game.make_move("i3", "d3")
+        game.make_move("i1", "f1")
         move2 = game.get_active_player()
         self.assertEqual(move2, "RED")
-        game.make_move("a1", "b1")
+        game.display_game()
+        game.make_move("a1", "e1")
         move3 = game.get_active_player()
+        game.display_game()
         self.assertEqual(move3, "BLACK")
+
     
-    def test02_vertical_up(self):
+    def test02_horizontal_move(self):
         "This test checks that the pieces can move up without obstructions."""
         game = HasamiShogiGame()
-        game.make_move("i3", "d3")
-        new_space = game.get_square_occupant(game.move_to_index("d3"))
-        self.assertEqual(new_space, "BLACK")
-        old_space = game.get_square_occupant(game.move_to_index("i3"))
-        self.assertEqual(old_space, "NONE")
+        game.make_move("i1", "f1")
+        game.make_move("a1", "e1")
+        # print(f"SHOULD BE: {game.get_active_player()}")
+        # checks to move right
+        game.make_move("f1", "f5")
+        game.make_move("e1", "e5")
+        game.display_game()
+        # checks to move left
+        game.make_move("f5", "f1")
+        game.make_move("e5", "e1")
+        # checks to move right but out of bounds
+        game.make_move("f1", "w2")
+        # checks to move left but out of bounds
+        game.make_move("f5", "f9")
+        game.make_move("i2", "e2")
+        game.display_game()
 
-#    def test03_vertical_down(self):
-#        "This test checks that the pieces can down without obstructions."""
-#        game = HasamiShogiGame()
-#        game.make_move("i3", "d3")
-#        game.make_move("a1", "i1")
-#        filled_space = game.get_square_occupant(game.move_to_index("i1"))
-#        game.display_game()
-#        self.assertEqual(filled_space, "BLACK")
+    def test03_vertical_up_obstructed(self):
+        "This test checks that the pieces can down without obstructions."""
+        game = HasamiShogiGame()
+        not_allowed = game.make_move("i1", "a1")
+        self.assertFalse(not_allowed)
+        # game.make_move("a1", "i1")
+        filled_space = game.get_square_occupant(game.move_to_index("i1"))
+        game.display_game()
+        self.assertEqual(filled_space, "BLACK")
     
     def test04_move_to_index(self):
         """This test checks and determines if the square is converted correctly to an index."""
@@ -39,9 +54,10 @@ class HasamiTests(unittest.TestCase):
         square = game.move_to_index("a1")
         self.assertEqual(square, "00")
         square = game.move_to_index("z0")
-        self.assertFalse
+        self.assertFalse(square)
         square = game.move_to_index("i9")
-        self.assertEqual(square, "00")
+        self.assertEqual(square, "88")
+
     
 
 if __name__ == '__main__':
